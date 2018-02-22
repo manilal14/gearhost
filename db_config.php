05b8dc5354c499cf45dd2b46e0042991b036b3a1
mani <manihereatcusat@gmail.com>
2018-02-22 20:51:52 +0530

    first commit

---
+++ db_config.php
@@ -0,0 +1,20 @@
+<?php 
+
+	$DB_HOST = "den1.mysql6.gear.host";
+	$DB_USER = "studentapp1";
+	$DB_PASS = "Qwerty@12";
+	$DB_NAME = "StudentApp";
+ 
+ // To check connection
+ /*$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+ 
+ //Checking if any error occured while connecting
+ if (mysqli_connect_errno()) 
+ {
+ 	echo "Failed to connect to the Database: " . mysqli_connect_error();
+ 	die();
+ }
+ else
+ 	echo "connection established";*/
+
+ ?>
